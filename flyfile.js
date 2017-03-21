6f4690ac67f2b79328b42736788ff22e16677720
Brandon Kennedy <bkennedy@marketamerica.com>
2017-03-21 16:28:28 -0400

    setup flyfile

---
+++ flyfile.js
@@ -0,0 +1,143 @@
+
+module.exports = {
+  *default(fly) {
+    yield fly
+      .start('clean')
+      .parallel([
+        'js',
+        'styles',
+        'templates',
+        'images'
+      ])
+  },
+
+  *watch(fly) {
+    yield fly
+      .start('clean')
+      .parallel([
+        'jswatch',
+        'styleswatch',
+        'templateswatch',
+        'imageswatch'
+      ])
+  },
+
+  *deploy(fly) {
+    yield fly
+      .start('default')
+      .parallel([
+        'jsdeploy',
+        'stylesdeploy'
+      ])
+  },
+
+  *clean(fly) {
+    yield fly.clear(`${__dirname}/dist`)
+  },
+
+
+  // js functions
+  *js(fly) {
+    yield fly
+      .source(`${__dirname}/app/**/*.js`)
+      .babel({
+        presets: ['es2015']
+      })
+      .target(`${__dirname}/dist`)
+  },
+
+  *jswatch(fly) {
+    yield fly
+      .start('js')
+      .watch(`${__dirname}/app/**/*.js`, ['js'])
+  },
+
+  *jsdeploy(fly) {
+    yield fly
+      .source(`${__dirname}/dist/**/*.js`)
+      .uglify()
+      .rename({
+        suffix: '.min'
+      })
+      .target(`${__dirname}/dist`)
+  },
+
+  *cleanjs(fly) {
+    yield fly.clear(`${__dirname}/dist/js`)
+  },
+
+
+  // style functions
+  *styles(fly) {
+    yield fly
+      .source(`${__dirname}/app/**/*.scss`)
+      .sass({
+        outputStyle: 'expanded'
+      })
+      .autoprefixer()
+      .target(`${__dirname}/dist`)
+  },
+
+  *styleswatch(fly) {
+    yield fly
+      .start('styles')
+      .watch(`${__dirname}/app/**/*.scss`, ['styles'])
+  },
+
+  *stylesdeploy(fly) {
+    yield fly
+      .source(`${__dirname}/dist/**/*.css`)
+      .postcss({
+        plugins: [
+          require('cssnano')
+        ]
+      })
+      .rename({
+        suffix: '.min'
+      })
+      .target(`${__dirname}/dist`)
+  },
+
+  *cleanstyles(fly) {
+    yield fly.clear(`${__dirname}/dist/styles`)
+  },
+
+
+  // template functions
+  *templates(fly) {
+    yield fly
+      .source(`${__dirname}/app/templates/**/*.pug`)
+      .pug({
+        pretty: true
+      })
+      .target(`${__dirname}/dist`)
+  },
+
+  *templateswatch(fly) {
+    yield fly
+      .start('templates')
+      .watch(`${__dirname}/app/templates/**/*.pug`, ['templates'])
+  },
+
+  *cleantemplates(fly) {
+    yield fly.clear(`${__dirname}/dist/templates`)
+  },
+
+
+  // image functions
+  *images(fly) {
+    yield fly
+      .source(`${__dirname}/app/images/**/*`)
+      .target(`${__dirname}/dist/images`)
+  },
+
+  *imageswatch(fly) {
+    yield fly
+      .start('images')
+      .watch(`${__dirname}/app/images/**/*`, ['images'])
+  },
+
+  *cleanimages(fly) {
+    yield fly.clear(`${__dirname}/dist/images`)
+  }
+}
